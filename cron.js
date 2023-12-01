0adf4586f4403e468aa394e1263030841fac3697
laluferdianyusuf <lferdianyusuf@gmail.com>
2023-12-01 08:28:00 +0800

    Done

---
+++ cron.js
@@ -0,0 +1,149 @@
+const cloudinary = require("./utils/cloudinary");
+const userRepository = require("./repositories/userRepository");
+const reportRepository = require("./repositories/reportRepository");
+
+const dailyCron = async (event, context) => {
+  console.log("daily schedule");
+  try {
+    const users = await userRepository.getUsers();
+
+    const reports = await reportRepository.getReportByType("HARIAN");
+    const nowDays = dayjs().tz("Asia/Makassar");
+
+    const createdAt = nowDays.format("dddd D MMMM YYYY HH:mm");
+
+    for (const user of users) {
+      if (user.division !== "supervisor") {
+        const saveToDB = await userRepository.saveToNewDb({
+          name: user.name,
+          nip: user.nip,
+          division: user.division,
+          email: user.email,
+          password: user.password,
+          phone_number: user.phone_number,
+          address: user.address,
+          role: user.role,
+          status: user.status,
+          days: createdAt,
+          recapType: "MINGGUAN",
+          userId: user.id,
+        });
+
+        if (saveToDB) {
+          const updatedStatus = await userRepository.updateUserStatus(
+            user.id,
+            "alpha",
+            "Weekly"
+          );
+          if (updatedStatus) {
+            for (const report of reports) {
+              const dailyUsers = await userRepository.getRecapUsers();
+              const monthlyUsers = await userRepository.getMonthlyRecapUsers();
+              await reportRepository.updateReportType(
+                report.id,
+                "MINGGUAN",
+                dailyUsers.id,
+                monthlyUsers.id
+              );
+            }
+          }
+          console.log("updated");
+        } else {
+          console.log("no data to save");
+        }
+      } else {
+        console.log("User is a Supervisor, skipping");
+      }
+    }
+
+    console.log("update successfully");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const weeklyCron = async (event, context) => {
+  console.log("weekly schedule");
+  try {
+    const users = await userRepository.getRecapUsers();
+    const reports = await reportRepository.getReportByType("MINGGUAN");
+
+    for (const user of users) {
+      const saveToDB = await userRepository.saveToMonthlyDb({
+        name: user.name,
+        nip: user.nip,
+        division: user.division,
+        email: user.email,
+        password: user.password,
+        phone_number: user.phone_number,
+        address: user.address,
+        role: user.role,
+        status: user.status,
+        days: user.days,
+        recapType: "BULANAN",
+        userId: user.id,
+      });
+      if (saveToDB) {
+        await userRepository.deleteUsers();
+      } else {
+        console.log("no data to save");
+      }
+    }
+    for (const report of reports) {
+      await reportRepository.updateReportType(report.id, "BULANAN");
+    }
+    console.log("updated");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const oneMonthInSeconds = 30 * 24 * 60 * 60;
+
+const monthlyCron = async (event, context) => {
+  console.log("monthly schedule");
+  try {
+    const user = await userRepository.getUsersByReportType("BULANAN");
+
+    for (const users of user) {
+      await userRepository.deleteMonthlyUsers();
+      await reportRepository.deleteReport("BULANAN");
+    }
+
+    const cloudinaryResources = await cloudinary.api.resources({
+      type: "upload",
+      max_results: 1000,
+      context: true,
+    });
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    for (const resource of cloudinaryResources.resources) {
+      const uploadTimestamp = Math.floor(
+        new Date(resource.created_at).getTime() / 1000
+      );
+      const ageInSeconds = currentTime - uploadTimestamp;
+
+      if (ageInSeconds > oneMonthInSeconds) {
+        const publicId = resource.public_id;
+
+        try {
+          const result = await cloudinary.uploader.destroy(publicId);
+          console.log(`Deleted from Cloudinary: ${publicId}`);
+        } catch (cloudinaryError) {
+          console.error("Error deleting from Cloudinary:", cloudinaryError);
+        }
+      }
+    }
+    console.log("successful");
+  } catch (error) {
+    console.log("error: " + error);
+    throw error;
+  }
+};
+
+module.exports = {
+  dailyCron,
+  weeklyCron,
+  monthlyCron,
+};
