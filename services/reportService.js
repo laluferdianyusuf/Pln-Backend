fd4bb6c6f06d3809d405a2234179f0dc732b6b79
laluferdianyusuf <lferdianyusuf@gmail.com>
2023-12-06 09:05:11 +0800

    FIxed

--- services/reportService.js
+++ services/reportService.js
@@ -120,7 +120,7 @@ class ReportService {
   static async getAllReports() {
     try {
       const reports = await reportRepository.getAllReports();
-      if (reports && reports.length > 0) {
+      if (reports) {
         const uniqueDates = Array.from(
           new Set(reports.map((report) => report.createdAt))
         );
